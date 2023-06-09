5003c56f8f9f8ea0c250421e74275c485615970d
Álvaro Braz <alvarobraz83@gmail.com>
2023-06-09 11:00:41 -0300

    fix: connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,3 @@
-// import { createConnection, Connection, getConnectionOptions } from 'typeorm';
-
-// export default async (host = 'localhost'): Promise<Connection> => {
-//   const defaultOptions = await getConnectionOptions();
-
-//   return createConnection(
-//     Object.assign(defaultOptions, {
-//       host,
-//     }),
-//   );
-// };
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (host = 'localhost'): Promise<Connection> => {
