87a74243dcbb649527977440e82f83b8fbc1ce21
Álvaro Braz <alvarobraz83@gmail.com>
2023-06-15 14:10:30 -0300

    fix: ormconfig more docker yml

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,18 +4,34 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+import '@shared/container';
 import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppErros';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import { router } from '@shared/infra/http/routes';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 
-import '@shared/container';
-
 createConnection();
-
 const app = express();
+
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -25,6 +41,8 @@ app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
